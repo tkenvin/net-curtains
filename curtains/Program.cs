8719d80db5f38b4e075a305d7fed58748b891c98
Thomas Kenvin <1545611+tkenvin@users.noreply.github.com>
2020-09-22 02:58:01 +0100

    Initial commit

---
+++ curtains/Program.cs
@@ -0,0 +1,60 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace curtains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userInput = new XkcdColour
+            {
+                Value = args[0]
+            };
+            var colourNames = ReadCsvInput(".\\rgb.txt");
+            var allOptions = colourNames
+                .Select(xc => new
+                {
+                    Proximity = xc.Proximity(userInput),
+                    Colour = xc
+                });
+            var smallestProx = allOptions
+                .Min(p => p.Proximity);
+            var closestOptions = allOptions
+                .Where(p => p.Proximity == smallestProx)
+                .Select(p => p.Colour);
+            foreach (var option in closestOptions)
+            {
+                Console.WriteLine($"{option.Name} {option.Value}");
+            }
+        }
+
+        static IEnumerable<XkcdColour> ReadCsvInput(string filename)
+        {
+            var xkcdColours = new List<XkcdColour>();
+            var config = new CsvConfiguration(CultureInfo.CurrentCulture)
+            {
+                HeaderValidated = null,
+                MissingFieldFound = null,
+                IgnoreBlankLines = true,
+                Delimiter = "\t"
+            };
+            using (var textReader = new StreamReader(filename))
+            using (var csvReader = new CsvReader(textReader, config))
+            {
+                var moreRecords = csvReader.Read();
+                while (moreRecords)
+                {
+                    xkcdColours.Add(csvReader.GetRecord<XkcdColour>());
+                    moreRecords = csvReader.Read();
+                }
+            }
+            return xkcdColours;
+        }
+    }
+}
---
+++ curtains/XkcdColour.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+
+namespace curtains
+{
+    class XkcdColour
+    {
+        [CsvHelper.Configuration.Attributes.Index(0)]
+        public string Name { get; set; }
+
+        [CsvHelper.Configuration.Attributes.Index(1)]
+        public string Value { get; set; }
+
+        private Color _colour;
+        private Color Colour
+        {
+            get
+            {
+                if (_colour == Color.Empty)
+                {
+                    _colour = ColorTranslator.FromHtml(Value);
+                }
+                return _colour;
+            }
+        }
+
+        private int Red => Colour.R;
+
+        private int Green => Colour.G;
+
+        private int Blue => Colour.B;
+
+        public int Proximity(XkcdColour other)
+        {
+            var redDiff = Math.Abs(this.Red - other.Red);
+            var greenDiff = Math.Abs(this.Green - other.Green);
+            var blueDiff = Math.Abs(this.Blue - other.Blue);
+            return redDiff + greenDiff + blueDiff;
+        }
+    }
+}
